6915587073dddb0e49a487d30b19f5d69329eccf
Max Nowack <max.nowack@gmail.com>
2022-02-10 13:47:59 +0100

    feat: add setLocaleSync and loadLocaleSync methods

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,38 @@ class LocaleManager extends EventEmitter {
     return this.locale
   }
 
+  public setLocaleSync(locale: string): void // eslint-disable-next-line lines-between-class-members
+  public setLocaleSync(locale: string, phrases: Record<string, string>): void // eslint-disable-next-line lines-between-class-members
+  public setLocaleSync(
+    locale: string,
+    fileOrMessages?: Record<string, string>,
+  ) {
+    if (!this.phraseCache[locale]) {
+      this.loadLocaleSync(locale, fileOrMessages)
+    }
+    this.locale = locale
+    this.emit('setLocale', locale)
+  }
+
   public setLocale(locale: string): Promise<void> // eslint-disable-next-line lines-between-class-members
   public setLocale(locale: string, phrases: Record<string, string>): Promise<void> // eslint-disable-next-line lines-between-class-members
   public async setLocale(
     locale: string,
     fileOrMessages?: Record<string, string>,
   ): Promise<void> {
-    if (!this.phraseCache[locale]) {
-      await this.loadLocale(locale, fileOrMessages)
-    }
-    this.locale = locale
-    this.emit('setLocale', locale)
+    this.setLocaleSync(locale, fileOrMessages as Record<string, string>)
+    return Promise.resolve()
   }
 
-  public async loadLocale(locale: string, fileOrMessages?: Record<string, string>) {
+  public async loadLocaleSync(locale: string, fileOrMessages?: Record<string, string>) {
     if (!fileOrMessages) {
       throw new Error(`Cannot load locale '${locale}' without filename or phrases provided`)
     }
     this.phraseCache[locale] = fileOrMessages
+  }
+
+  public async loadLocale(locale: string, fileOrMessages?: Record<string, string>) {
+    this.loadLocaleSync(locale, fileOrMessages)
     return Promise.resolve()
   }
 
@@ -67,4 +81,6 @@ export default ubersetz
 export const getLocale = manager.getLocale.bind(manager)
 export const setLocale = manager.setLocale.bind(manager)
 export const loadLocale = manager.loadLocale.bind(manager)
+export const setLocaleSync = manager.setLocaleSync.bind(manager)
+export const loadLocaleSync = manager.loadLocaleSync.bind(manager)
 export const onLocaleChange = manager.on.bind(manager, 'setLocale')
